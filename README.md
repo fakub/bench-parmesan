72154e58d0f8e8030d3cd10f1f4e8f8e9dd21889
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2021-08-12 23:47:14 +0200

    Notes in README, idea of benchmarks.

--- README.md
+++ README.md
@@ -7,4 +7,6 @@
 * all threads are guaranteed to be on single physical processor?
 * set highest CPU clock?
     * `-l select=cpu_flag=<some-turbo-flag>` ?
-    * other settings on [wiki](https://wiki.metacentrum.cz/wiki/About_scheduling_system)
+    * maybe [only one core](https://en.wikichip.org/wiki/intel/xeon_platinum/8280)?
+* 2 or 3 [UPI links](https://en.wikichip.org/wiki/intel/microarchitectures/cascade_lake)?
+* other settings on [wiki](https://wiki.metacentrum.cz/wiki/About_scheduling_system)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use colored::Colorize;
 
-
-
 fn main() {
 
     parmesan::simple_duration!(
@@ -12,7 +10,56 @@ fn main() {
     //TODO  generate a set of random messages of different lengths (possibly directly from alphabet),
     //      benchmark addition, subtraction, signum, maximum, multiplication, (scalar multiplication, NN evaluation)
 
-    let _p = parmesan::arith_demo();
 
-    let _a = parmesan::nn_demo();
+    // =========================================================================
+    //  Generate & Encrypt inputs
+
+    // encrypt 4 random 32-word sequences of {-1,0,1}
+
+
+    // =========================================================================
+    //  Addition
+
+    // first-level addition:   a + b   ,   c + d
+
+    // second level addition   (a+b) + (c+d)
+
+
+    // =========================================================================
+    //  Signum
+
+    // signum of fresh
+
+    // signum of result
+
+
+    // =========================================================================
+    //  Maximum
+
+    // first-level maximum
+
+    // second-level maximum
+
+
+    // =========================================================================
+    //  Multiplication
+
+    // 4-word multiplication -> 8-word (congruent mod 2^8, can have negative sign, not good for comparison)
+
+    // 8-word multiplication -> 16-word (...)
+
+    // 16-word multiplication -> 32-word (...)
+
+    // 32-word multiplication -> 64-word (...)
+
+
+    // =========================================================================
+    //  TODO: Scalar Multiplication
+
+    // 4-bit scalar times 4-word ciphertext
+
+    // 8-bit scalar times 8-word ciphertext
+
+    // 16-bit scalar times 16-word ciphertext
+
 }
