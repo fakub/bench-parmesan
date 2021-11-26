8fb34971be882474084cfb6f42cf4cb6872d882b
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2021-11-26 13:39:30 +0100

    Plotting: time of operations taken from file.

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -13,37 +13,25 @@ set boxwidth 0.025
 set multiplot layout 2,1
 
 set yrange [-5:110]
-set xrange [0.0:75.0]
+set xrange [0.5:74.0]
 
-set xtics 0,1,75 nomirror
-set x2tics ( \
-    'Load keys' 02.016, \
-    'PBS (10x)' 07.367, \
-    '1st level add' 08.340, \
-    '1st level subtr' 09.178, \
-    '2nd level add' 10.005, \
-    'sgn 1' 10.905, \
-    'sgn 2' 12.116, \
-    'max 1' 13.478, \
-    'max 2' 17.957, \
-    'max 3' 22.202, \
-    '4w mul' 26.814, \
-    '8w mul' 28.807, \
-    '4w squ' 37.066, \
-    '8w squ' 38.951, \
-    'sc mul 161' 45.884, \
-    'sc mul 239' 48.760, \
-    'sc mul 231' 51.475, \
-    'sc mul 170' 55.369, \
-    'sc mul 219' 59.154, \
-    'nn eval' 63.065, \
-    'END' 71.736, \
-) rotate by 45 left
+set ytics 0,25,100
+
+# combine files: https://stackoverflow.com/questions/11549004/gnuplot-plot-data-from-two-files-in-one-x-coordinate-in-other-y
+# custom xtic: https://stackoverflow.com/questions/50340829/gnuplot-using-xtic-from-data-but-plotting-only-every-10th
 
 # skip first two lines
-cpu_stats = "\"< awk '(NR>2){print;}' cpu-stats.log\""
+cpu_stats = "\"< paste cpu-stats.log fakubitsu_desired-format.log | awk '(NR>2){print;}'\""
 
 # ------------------------------------------------------------------------------
+
+set tmargin at screen 0.48
+set bmargin at screen 0.88
+
+unset xtics
+set x2tics rotate by 45 left
+
+set grid x2tics ytics dt 3 lw 1 lt -1
 
 plot \
     @cpu_stats u ($6-.0375):1 w boxes t 'CPU 0 load', \
@@ -54,20 +42,26 @@ plot \
     @cpu_stats u 6: 8 w l dt 3 lw 2 t 'CPU 1 freq.', \
     @cpu_stats u 6: 9 w l dt 3 lw 2 t 'CPU 2 freq.', \
     @cpu_stats u 6:10 w l dt 3 lw 2 t 'CPU 3 freq.', \
-    0   w l dt 3 lw 1 lt -1 t '', \
-    100 w l dt 3 lw 1 lt -1 t ''
+    @cpu_stats u 11:(-100.0):x2tic(12) notitle
+    # hack: -100.0 puts the value far away, how to make it invisible?
 
 # ------------------------------------------------------------------------------
 
-set ytics 0,25,100
+set tmargin at screen 0.08
+set bmargin at screen 0.48
+
+set xtics 0,1,75 nomirror
+# keep x2tics
+
+set xlabel 'Time [s]'
 
 plot \
     @cpu_stats u 6:(($1+$2+$3+$4 )/4) w boxes t 'Total CPU load', \
     @cpu_stats u 6:(($7+$8+$9+$10)/4) w l dt 3 lw 2 t 'Avg. CPU freq.', \
-    0   w l dt 3 lw 1 lt -1 t '', \
-    25  w l dt 3 lw 1 lt -1 t '', \
-    50  w l dt 3 lw 1 lt -1 t '', \
-    75  w l dt 3 lw 1 lt -1 t '', \
-    100 w l dt 3 lw 1 lt -1 t ''
+    @cpu_stats u 11:(-100.0):x2tic("") notitle
 
     #~ @cpu_stats u (column(0)-.375):($1) w boxes t 'CPU 0 load', \
+
+# ------------------------------------------------------------------------------
+
+unset multiplot
